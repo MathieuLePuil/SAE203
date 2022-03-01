db4b8fa5ad9c488d951af091ceb3d54ba2db53e8
MathieuLPL <lepuilmathieu@gmail.com>
2022-03-01 14:53:23 +0100

    Ajout du menu de navigation responsive

--- debut.php
+++ debut.php
@@ -4,3 +4,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <script src="https://kit.fontawesome.com/10e721539d.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/LOGO.png">
--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <?php
     require("debut.php");
 ?>
-<title>Plastic Duck | Home</title>
+<title>ATP Tennis | Home</title>
 
 <body>
+    <header>
+        <div class="logotxt logo">
+            <img src="assets/LOGO.png" alt="Logo de l'ATP" id="logohg" width="50" height="50">
+        </div>
+        <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <i class="fa-solid fa-bars"></i>
+            </label>
+            <a href="http://www.mmi21b12.sae203.ovh/">
+                <label class="logo">Tennis</label></a>
+            <ul>
+                <li><a href="index.php" class="active anav">Accueil</a></li>
+                <li><a href="atp.php" class="anav">l'ATP</a></li>
+                <li><a href="admin.php" class="anav" target="_blank">Administrateur</a></li>
+                <li><a href="recherche.php" class="anav">Recherche</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <h1>Plastic Duck</h1>
-    <h3>Le site de canard en plastique</h3>
+    <h1>ATP Tennis</h1>
+    <h3>Le site du classement et des tournois de l'ATP</h3>
 
 <?php
     require("fin.php");
