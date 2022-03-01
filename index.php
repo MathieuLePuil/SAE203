69020fab9205a0d91e7490892e5690ec139975f0
MathieuLPL <lepuilmathieu@gmail.com>
2022-03-01 14:12:47 +0100

    Premier commit :
    - debut.php
    - index.php
    - fin.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    require("debut.php");
+?>
+<title>Plastic Duck | Home</title>
+
+<body>
+
+    <h1>Plastic Duck</h1>
+    <h3>Le site de canard en plastique</h3>
+
+<?php
+    require("fin.php");
+?>
