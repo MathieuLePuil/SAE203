8921394ce10d6c52576e9c1f665cab21fb7671db
MathieuLPL <lepuilmathieu@gmail.com>
2022-03-04 11:51:25 +0100

    Auto-Save

--- autosavesae203.bat
+++ autosavesae203.bat
@@ -1,5 +1,5 @@
 cd C:/Users/MathieuLP/Desktop/B.U.T_MMI/COURS/Semestre_2/SAE/SAE203/SAE203
 git add .
-git commit -m 'Auto-Save'
+git commit -m Auto-Save
 git push
 ssh mmi21b12@mmi21b12.mmi-troyes.fr ./pullrequest.sh
